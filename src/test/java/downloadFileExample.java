8937dff0fbd8271863860848b362061e197b30c4
PrasadiLanka <lankasilva610@gmail.com>
2025-02-27 10:01:48 +0530

    file upload

--- src/test/java/downloadFileExample.java
+++ src/test/java/downloadFileExample.java
@@ -5,6 +5,9 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
+import java.awt.*;
+import java.awt.datatransfer.StringSelection;
+import java.awt.event.KeyEvent;
 import java.io.File;
 
 public class downloadFileExample {
@@ -36,5 +39,43 @@ public class downloadFileExample {
             }
 
         }
+    }
+
+    @Test
+    public void fileUpload() throws AWTException, InterruptedException {
+        driver.get("https://www.leafground.com/file.xhtml");
+
+
+//        //1st way - using robot class (issue with linux OS)
+//        WebElement uploadBTn = driver.findElement(By.id("j_idt88:j_idt89"));
+//        uploadBTn.click();
+//
+//        /*after this part open the laptop's window(linux)then that is can not handle by the
+//        selenium.to handle that part should use the java.*/
+//
+//        StringSelection selection = new StringSelection("/home/lanka/Downloads/pp_princess_snowwhite_static_mobile_20694_65502630.jpeg");
+//        Toolkit.getDefaultToolkit().getSystemClipboard().setContents(selection,null);    /*before upload the file should copy the path that going to upload.that path should copied
+//                                                                                                  to clipboard.this line for that.this setContents only accept transferablecontents.because we can't send
+//                                                                                                   string values directly.*/
+//
+//        Robot robot = new Robot();  /* should declare this robot class to work with keyboard actions(paste that copied path). we can't use Actions class for this.
+//                                    Actions class methods only can use for web driver or selenium things. now this is not connect with selenium until this works as OS
+//                                    application.using this class can perform keyboard actions*/
+//        Thread.sleep(3000);
+//        robot.keyPress(KeyEvent.VK_CONTROL);
+//        robot.keyPress(KeyEvent.VK_V);
+//        robot.keyRelease(KeyEvent.VK_V);
+//        robot.keyRelease(KeyEvent.VK_CONTROL);
+//
+//        Thread.sleep(3000);
+//        robot.keyPress(KeyEvent.VK_ENTER);
+//        robot.keyRelease(KeyEvent.VK_ENTER);
+
+
+
+        //2nd way - using send keys (Applicable only element type is file.that means the locator of upload button, html element input type = "file" )
+        String data = "/home/lanka/Downloads/pp_princess_snowwhite_static_mobile_20694_65502630.jpeg";
+        WebElement uploadUsingSendKeys = driver.findElement(By.id("j_idt88:j_idt89_input"));
+        uploadUsingSendKeys.sendKeys(data);
     }
 }
